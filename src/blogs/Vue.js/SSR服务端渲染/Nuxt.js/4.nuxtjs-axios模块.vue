87af8c6921274741fd1dc07f63f246f3f1d622cf
710241969 <710241969@qq.com>
2018-04-17 21:08:14 +0800

    添加博客

---
+++ src/blogs/Vue.js/SSR服务端渲染/Nuxt.js/4.nuxtjs-axios模块.vue
@@ -0,0 +1,30 @@
+<template>
+    <pre>
+      <textarea>
+# 标题
+
+```js
+modules: [
+        '@nuxtjs/axios',
+        '@nuxtjs/proxy'
+],
+......
+```
+或者
+```js
+modules: [
+    '@nuxtjs/axios',
+],
+axios: {
+    proxy: true
+},
+......
+```
+      </textarea>
+  </pre>
+</template>
+
+<script>
+export default {};
+</script>
+<style lang="scss" scoped></style>
---
+++ src/blogs/Vue.js/SSR服务端渲染/Nuxt.js/5.nuxt-koa前后端分离.vue
@@ -0,0 +1,13 @@
+<template>
+  <pre>
+# 标题
+
+默认3000端口
+如果要更换端口，必须在package.json中修改端口
+  </pre>
+</template>
+
+<script>
+export default {};
+</script>
+<style lang="scss" scoped></style>
